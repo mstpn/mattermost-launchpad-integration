80aff9a75b3df2cd30667845110a68d01761632e
Matthew Stepan <matthew.stepan@canonical.com>
2024-05-17 01:57:46 -0600

    demo build

--- mattermostgithub/server.py
+++ mattermostgithub/server.py
@@ -18,6 +18,8 @@ from mattermostgithub.payload import (
 from mattermostgithub import app
 
 SECRET = hmac.new(config.SECRET.encode('utf8'), digestmod=hashlib.sha1) if config.SECRET else None
+EVENTS = {'git:push:0.1': 'Git push', 'merge-proposal:0.1': 'Merge proposal', 'bug:0.1': 'Bug', 'bug:comment:0.1': 'Bug Comment'}
+MP_ATTRIBUTES = ['registrant', 'source_branch', 'source_git_repository', 'source_git_path', 'target_branch', 'target_git_repository', 'target_git_path', 'prerequisite_branch', 'prerequisite_git_repository', 'prerequisite_git_path', 'queue_status', 'commit_message', 'whiteboard', 'description', 'preview_diff', 'preview_diff']
 
 @app.route(config.SERVER['hook'] or "/", methods=['POST'])
 def root():
@@ -25,109 +27,73 @@ def root():
         print('Invalid Content-Type')
         return 'Content-Type must be application/json and the request body must contain valid JSON', 400
 
-    if SECRET:
-        signature = request.headers.get('X-Hub-Signature', None)
-        sig2 = SECRET.copy()
-        sig2.update(request.data)
-
-        if signature is None or sig2.hexdigest() != signature.split('=')[1]:
-            return 'Invalid or missing X-Hub-Signature', 400
+    # if SECRET:
+    #     signature = request.headers.get('X-Hub-Signature', None)
+    #     sig2 = SECRET.copy()
+    #     sig2.update(request.data)
+    #
+    #     print('secret:', sig2.hexdigest())
+    #     print('signature:', signature)
+    #
+    #     if signature is None or sig2.hexdigest() != signature.split('=')[1]:
+    #         return 'Invalid or missing X-Hub-Signature', 400
 
     data = request.json
-    event = request.headers['X-Github-Event']
+    headers = request.headers
+    # print(headers)
+    print(data)
+    
+    event = EVENTS.get(headers.get('X-Launchpad-Event-Type'))
+    if event is None:
+        event = "Unknown"
+    print(event)
 
     msg = ""
-    if event == "ping":
-        msg = "ping from %s" % data['repository']['full_name']
-    elif event == "pull_request":
-        if data['action'] == "opened":
-            msg = PullRequest(data).opened()
-        elif data['action'] == "closed":
-            msg = PullRequest(data).closed()
-        elif data['action'] == "assigned":
-            msg = PullRequest(data).assigned()
-        elif data['action'] == "synchronize":
-            msg = PullRequest(data).synchronize()
-        elif data['action'] == "review_requested":
-            msg = PullRequest(data).review_requested()
-        elif data['action'] == "enqueued":
-            msg = PullRequest(data).pr_enqueued()
-        elif data['action'] == "dequeued":
-            msg = PullRequest(data).pr_dequeued()
-    elif event == "issues":
-        if data['action'] == "opened":
-            msg = Issue(data).opened()
-        elif data['action'] == "closed":
-            msg = Issue(data).closed()
-        elif data['action'] == "labeled":
-            msg = Issue(data).labeled()
-        elif data['action'] == "assigned":
-            msg = Issue(data).assigned()
-    elif event == "issue_comment":
-        if data['action'] == "created":
-            msg = IssueComment(data).created()
-    elif event == "repository":
-        if data['action'] == "created":
-            msg = Repository(data).created()
-    elif event == "create":
-        if data['ref_type'] == "branch":
-            msg = Branch(data).created()
-        elif data['ref_type'] == "tag":
-            msg = Tag(data).created()
-    elif event == "delete":
-        if data['ref_type'] == "branch":
-            msg = Branch(data).deleted()
-    elif event == "pull_request_review":
-        if data['action'] == "submitted":
-            msg = PullRequestReview(data).submitted()
-    elif event == "pull_request_review_comment":
-        if data['action'] == "created":
-            msg = PullRequestComment(data).created()
-    elif event == "push":
-        if not (data['deleted'] and data['forced']):
-            if not data['ref'].startswith("refs/tags/"):
-                msg = Push(data).commits()
-    elif event == "commit_comment":
-        if data['action'] == "created":
-            msg = CommitComment(data).created()
-    elif event == "gollum":
-        msg = Wiki(data).updated()
-    elif event == "status":
-        if data["state"] in ["failure", "error"]:
-            msg = Status(data).updated()
+    if event == 'Git push':
+        path = data.get("git_repository_path", None)
+        ref = data.get("ref_changes", None)
+        if path is None or ref is None:
+            return "Invalid event"
+        repo_url = 'https://launchpad.net/' + path
+        branch = list(ref.keys())[0].split('/')[-1]
+        branch_url = repo_url + '/+ref/' + branch
+        commit_hash = ref.get(f'refs/heads/{branch}').get('new').get('commit_sha1')
+        commit_url = 'https://git.launchpad.net/~mstepan/+git/tmp/commit/?id=' + commit_hash
+        msg = f'[Changes]({commit_url}) pushed to [`{branch}`]({branch_url}) at [`{path}`]({repo_url})'
+    elif event == 'Merge proposal':
+        mp_url = 'https://code.launchpad.net' + data.get('merge_proposal')
+        if data.get('action') == 'created':
+            attributes = data.get('new')
+            registrant = attributes.get('registrant')
+            registrant_url = 'https://launchpad.net' + registrant
+            source_repo = attributes.get('source_git_repository')
+            source_branch = attributes.get('source_git_path').split('/')[-1]
+            source_url = 'https://launchpad.net' + source_repo + '/+ref/' + source_branch
+            target_repo = attributes.get('target_git_repository')
+            target_branch = attributes.get('target_git_path').split('/')[-1]
+            target_url = 'https://launchpad.net' + target_repo + '/+ref/' + target_branch
+            msg = f'[`{registrant[1:]}`]({registrant_url}) has [proposed merging]({mp_url}) [`{source_repo[1:]}`]({source_url}) into [`{target_repo[1:]}`]({target_url})'
+        elif data.get('action') == 'modified':
+            # Todo: get diff between new and old -> print it
+            diff = []
+            new = data.get('new')
+            old = data.get('old')
+            for attribute in MP_ATTRIBUTES:
+                if new[attribute] != old[attribute]:
+                    diff.append(f'{attribute}: {old[attribute]} -> {new[attribute]}')
+            diff_str = '\n\t-' + '\n\t-'.join(diff)
+            msg = f'[`{data.get("merge_proposal")[1:]}`]({mp_url}) modified: {diff_str}'
+            # msg = 'modified'
+        elif data.get('action') == 'deleted':
+            msg = f'{mp_url} has been deleted'
+        else:
+            msg = 'Merge proposal'
 
     if msg:
-        hook_info = get_hook_info(data)
-        if hook_info:
-            url, channel = get_hook_info(data)
-
-            action = None
-            if "action" in data:
-                action = data["action"]
-            elif "ref_type" in data:
-                action = data["ref_type"]
-
-            if action:
-                if hasattr(config, "GITHUB_IGNORE_ACTIONS") and \
-                   event in config.GITHUB_IGNORE_ACTIONS and \
-                   action in config.GITHUB_IGNORE_ACTIONS[event]:
-                    return "Notification action ignored (as per configuration)"
-
-            if hasattr(config, "IGNORE_USER_EVENTS") and "sender" in data \
-               and data['sender']['login'] in config.IGNORE_USER_EVENTS \
-               and event in config.IGNORE_USER_EVENTS[data['sender']['login']]:
-                return "User blocked from generating this notifications"
-
-            if hasattr(config, "REDIRECT_EVENTS") and \
-                    event in config.REDIRECT_EVENTS:
-                channel = config.REDIRECT_EVENTS[event]
-
-            post(msg, url, channel)
-            return "Notification successfully posted to Mattermost"
-        else:
-            return "Notification ignored (repository is blacklisted)."
-    else:
-        return "Not implemented", 400
+        url, channel = config.MATTERMOST_WEBHOOK_URLS['default']
+        post(msg, url, channel)
+        return "Notification successfully posted to Mattermost"
+    return ""
 
 def post(text, url, channel):
     data = {}
@@ -141,26 +107,6 @@ def post(text, url, channel):
 
     if r.status_code is not requests.codes.ok:
         print('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (url, r.status_code, r.json()))
-
-def get_hook_info(data):
-    if 'repository' in data:
-        repo = data['repository']['full_name']
-        if repo in config.MATTERMOST_WEBHOOK_URLS:
-            return config.MATTERMOST_WEBHOOK_URLS[repo]
-    if 'organization' in data:
-        org = data['organization']['login']
-        if org in config.MATTERMOST_WEBHOOK_URLS:
-            return config.MATTERMOST_WEBHOOK_URLS[org]
-    if 'repository' in data:
-        if 'login' in data['repository']['owner']:
-            owner = data['repository']['owner']['login']
-            if owner in config.MATTERMOST_WEBHOOK_URLS:
-                return config.MATTERMOST_WEBHOOK_URLS[owner]
-        if 'name' in data['repository']['owner']:
-            owner = data['repository']['owner']['name']
-            if owner in config.MATTERMOST_WEBHOOK_URLS:
-                return config.MATTERMOST_WEBHOOK_URLS[owner]
-    return config.MATTERMOST_WEBHOOK_URLS['default']
 
 if __name__ == "__main__":
     app.run(
